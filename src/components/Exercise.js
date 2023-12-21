1362693912ff5cf1d3aad4f47992f9bf1f81f5c6
Matt Orgon <matthewaorgon@gmail.com>
2023-12-21 13:24:49 -0800

    useEffect works

--- src/components/Exercise.js
+++ src/components/Exercise.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from '@emotion/styled';
 import exerciseGif from '../assets/Bicep-Curl-1gif.gif'
 import lockedImage from '../assets/illustration-of-lock-icon-vector.jpg'
@@ -51,9 +51,17 @@ const ExerciseCard = ({exerciseName}) => {
     const [isPressed, setIsPressed] = useState(false);
 
     const handleButtonClick = () => {
+        console.log('Before setIsPressed:', isPressed);
+        
         setIsPressed(!isPressed);
+        console.log('After setIsPressed:', isPressed);
     };
 
+    useEffect(() => {
+        // This will run every time isPressed changes
+        console.log('Effect triggered. Current value of isPressed:', isPressed);
+      }, [isPressed]);
+      
     
 
   return (
